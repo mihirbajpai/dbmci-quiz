916901e40e01a04b194ba8a8e09e1b3694c0c26d
Mihir <themihirbajpai@gmail.com>
2026-07-04 19:25:03 +0530

    extract lottie

---
+++ app/src/main/java/com/example/dbmciquiz/view/component/LottiePlayer.kt
@@ -0,0 +1,31 @@
+package com.example.dbmciquiz.view.component
+
+import androidx.annotation.RawRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+/** Renders a raw-res Lottie. When [isPlaying] is false it holds the first frame. */
+@Composable
+fun LottiePlayer(
+    @RawRes res: Int,
+    modifier: Modifier = Modifier,
+    iterations: Int = 1,
+    isPlaying: Boolean = true
+) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(res))
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = iterations,
+        isPlaying = isPlaying
+    )
+    LottieAnimation(
+        composition = composition,
+        progress = { progress },
+        modifier = modifier
+    )
+}
